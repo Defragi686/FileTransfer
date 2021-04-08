b02f6148d5988b9be48a3196855519ffbb266846
Kanui <65466003+Kanui-C@users.noreply.github.com>
2021-04-08 10:42:19 -0300

    Update servidor.c

--- servidor.c
+++ servidor.c
@@ -5,19 +5,24 @@
 #include <string.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <errno.h>
 //Constantes
-#define mbytes 1024
+#define mbytes 20024
 
-
+int recv_data(int socket, char *namefile, int op);
 int main(int argc, char *argv[]){
-    if(argc < 2){
-        printf("[ERRO] - Use: %s PORTA ARQUIVO\n", argv[0]);
+    int op = 0;
+    if(argc < 3){
+        printf("[ERRO] - Use: %s PORTA (OUTPUT)ARQUIVO\nPara transferencia de imagem Use -i", argv[0], argv[0]);
         return EXIT_FAILURE;
     }
+    for(int i = 0;i < argc;i++){
+        if (strcmp("-i", argv[i]) == 0) {
+            op = 1;
+       }
+    }
     int PORTA = atoi(argv[1]);
-    int skt, acptskt;
-    char buffer[mbytes];
-    FILE *transferfile;
+    int skt, acptskt,check_recv;
     char *namefile = argv[2];
     struct sockaddr_in svaddr,client;
     socklen_t sizeclient = sizeof(client);
@@ -36,15 +41,55 @@ int main(int argc, char *argv[]){
     listen(skt, 2);
 
     acptskt = accept(skt, (struct sockaddr *)&client, &sizeclient);
-
-    transferfile = fopen(argv[2], "w");
-
-    while ( (recv(acptskt,buffer,mbytes,0)) > 0 ){
-       printf("%s", buffer);
-       fprintf(transferfile, "%s", buffer);
-       memset(&buffer,'\0',sizeof(buffer));
+    if(acptskt < 0){
+        printf("[ERRO] Ocorreu um erro ao aceitar a conexao!");
+        return 0;
     }
-    printf("Arquivo recebido!\nSalvo como: %s",argv[2]);
+    check_recv = recv_data(acptskt, argv[2], op);
+
+    if(check_recv == 0){
+        printf("Arquivo recebido!\nSalvo como: %s",argv[2]);
+    }
     close(skt);
+    return 0;
+}
+
+int recv_data(int socket, char *namefile, int op){
+    FILE *transferfile;
+    int recv_size = 0,size = 0,status,write_s,ler_size;
+    char servidormsg[] = "[SERVIDOR] A Conexao foi estabelecida!\n";
+    char buffer[mbytes];
+
+    status = recv(socket, &size, sizeof(size), 0);
+    if(status < 0){
+        printf("[ERRO] Ocorreu um erro ao receber o tamanho do arquivo!\n");
+        return 0;
+    }
+    printf("Tamanho da imagem: %d\n",size);
+
+    status = send(socket, servidormsg, sizeof(servidormsg), 0);
+    if(status < 0){
+        printf("[ERRO] Ocorreu um erro no envio da mensagem:%s\n", servidormsg);
+    }
+
+    if( (transferfile = fopen(namefile, "w")) == NULL){
+        printf("[ERRO] Ocorreu um erro na tentativa de escrita do arquivo\n");
+        return EXIT_FAILURE;
+    }
+
+    while(size > recv_size){
+        ler_size = recv(socket,buffer,sizeof(buffer), 0);
+        if(recv_size < 0){
+            printf("%s", strerror(errno));
+        }
+        if(op == 1)
+            write_s = fwrite(buffer,1,sizeof(buffer), transferfile);
+        else
+            fprintf(transferfile, "%s", buffer);
+            
+        recv_size += ler_size;
+    }
+
+    fclose(transferfile);
     return 0;
 }
